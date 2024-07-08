15d08c86e293f9662bd22f1d2342844753dbe44d
Rodrigo Canales <canalesdesign2024@gmail.com>
2024-07-08 11:05:54 -0300

    Back GTM to layout-tsx

--- components/atoms/head.tsx
+++
@@ -1,8 +0,0 @@
-import { GoogleTagManager } from "@next/third-parties/google";
-export default function Head() {
-  return (
-    <head>
-      <GoogleTagManager gtmId="GTM-PPRTNJ8C" />
-    </head>
-  );
-}
