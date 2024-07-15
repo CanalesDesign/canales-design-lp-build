27edce94c1ce55ed46c1682b18562afe5a435113
Rodrigo Canales <canalesdesign2024@gmail.com>
2024-07-14 21:31:51 -0300

    Add canonical link

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
   },
+  alternates: {
+    canonical: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+  },
   openGraph: {
     title: "Canales – Design Centrado na Essência",
     description: "Identidade Visual que transforma empresas.",
